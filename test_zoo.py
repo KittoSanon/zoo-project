4184e0c251758b061af99afe61378968facf6127
KittoSanon <kittosanon@gmail.com>
2024-11-14 19:18:04 +0000

    Added test cases

--- test_zoo.py
+++ test_zoo.py
@@ -10,15 +10,22 @@ class TestZoo(unittest.TestCase):
 
     # Add your additional test cases here.
 
-    def test_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(10), 50)
-        self.assertEqual(self.zoo.get_ticket_price(20), 100)
-        self.assertEqual(self.zoo.get_ticket_price(45), 150)
-        self.assertEqual(self.zoo.get_ticket_price(21), 150)
-        self.assertEqual(self.zoo.get_ticket_price(55), 150)
-        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    def test_ticket1_price(self):
         self.assertEqual(self.zoo.get_ticket_price(0), 50)
-        self.assertEqual(self.zoo.get_ticket_price(16), 100)
+        
+    def test_ticket2_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_ticket3_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_ticket4_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_ticket5_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), None)
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+        self.assertEqual(self.zoo.get_ticket_price(55), 150)
 
 if __name__ == '__main__':
     unittest.main()
